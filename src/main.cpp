060bbaa01f7161f9a7bca9924fcedddc566b10a2
Richard Dijoux <richard.dijoux@lumapps.com>
2024-03-16 18:06:15 +0100

    feat(dep): add spdlog as dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
 
-#include "core/team.h"
 #include "core/tournament.h"
 
 int main(int, char **)
 {
+    spdlog::info("Welcome to TORNO!");
     Tournament tournament;
-    std::cout << "Hello, from torno!\n";
-
-    Team team1("Team 1");
-    std::cout << "Teams1: " << team1.get_name() << std::endl;
-
     return 0;
 }
